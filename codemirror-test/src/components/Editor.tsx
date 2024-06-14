070ea55c40f7b1a1262a98ff9db85a318f751be0
KHashimoto3 <hashi.koki385@gmail.com>
2024-06-14 17:13:23 +0900

    cppのハイライトに対応した。

--- codemirror-test/src/components/Editor.tsx
+++ codemirror-test/src/components/Editor.tsx
@@ -1,5 +1,6 @@
 import ReactCodeMirror from "@uiw/react-codemirror";
 import { SetStateAction, useCallback, useState } from "react";
+import { cppLanguage } from "@codemirror/lang-cpp";
 
 export const Editor = () => {
   const [value, setValue] = useState<string>("");
@@ -9,7 +10,11 @@ export const Editor = () => {
   return (
     <div>
       <h1>Editor</h1>
-      <ReactCodeMirror value={value} onChange={onChange} />
+      <ReactCodeMirror
+        value={value}
+        onChange={onChange}
+        extensions={[cppLanguage]}
+      />
       <p>入力内容：{value}</p>
     </div>
   );

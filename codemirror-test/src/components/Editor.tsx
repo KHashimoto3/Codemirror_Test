691151c7b476658b926039b353f1d31d2eacdae1
KHashimoto3 <hashi.koki385@gmail.com>
2024-06-14 16:56:01 +0900

    react-codemirrorを導入した。

--- codemirror-test/src/components/Editor.tsx
+++ codemirror-test/src/components/Editor.tsx
@@ -1,8 +1,15 @@
+import ReactCodeMirror from "@uiw/react-codemirror";
+import { SetStateAction, useCallback, useState } from "react";
+
 export const Editor = () => {
+  const [value, setValue] = useState<string>("");
+  const onChange = useCallback((val: SetStateAction<string>) => {
+    setValue(val);
+  }, []);
   return (
     <div>
       <h1>Editor</h1>
-      <p>Codemirror 6</p>
+      <ReactCodeMirror value={value} onChange={onChange} />
     </div>
   );
 };
